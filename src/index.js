e670df3a88e180c9ed7b68345ae83a50356575fe
JesCT-U <jesct2031@mesoamericana.edu.gt>
2022-06-20 17:13:31 -0600

    Bootstrap

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.set('views', join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(router)
 
-app.listen(3000)
+app.use(router)
+app.use(express.static(join(__dirname, 'public')))
+
+app.listen(process.env.PORT || 3000)
